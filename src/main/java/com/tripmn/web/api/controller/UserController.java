fc9bbeb9ddf5d9e9a7ff4bb50cdf8ae73fefd740
krishna <enkrao@gmail.com>
2015-11-29 16:20:05 +0530

    added fetchProfile in controller

--- src/main/java/com/tripmn/web/api/controller/UserController.java
+++ src/main/java/com/tripmn/web/api/controller/UserController.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.tripmn.dto.AuthenticationRequest;
 import com.tripmn.dto.AuthenticationResponse;
 import com.tripmn.dto.FetchItemsResponse;
 import com.tripmn.dto.FetchTokensResponse;
+import com.tripmn.dto.UserFetchProfileRequest;
+import com.tripmn.dto.UserFetchProfileResponse;
 import com.tripmn.dto.UserRegistrationRequest;
 import com.tripmn.dto.UserRegistrationResponse;
 import com.tripmn.service.ItemService;
@@ -49,5 +52,12 @@ public class UserController {
 	@RequestMapping("/fetchItems")
 	public @ResponseBody FetchItemsResponse fetchItems(){
 		return itemService.fetchItems();
+	}
+	
+	@RequestMapping("/fetchProfile")
+	public @ResponseBody UserFetchProfileResponse fetchProfile(
+			@RequestBody UserFetchProfileRequest userFetchProfileRequest) {
+
+		return userService.fetchProfile(userFetchProfileRequest);
 	}
 }
