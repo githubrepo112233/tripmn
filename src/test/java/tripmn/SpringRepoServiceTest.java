7c3fe241c6e01499c617588a7c32a86466aa5a3b
friendsforever <friendsforever>
2015-11-29 15:00:35 +0530

    date format change in fetch tokens Api

--- src/test/java/tripmn/SpringRepoServiceTest.java
+++ src/test/java/tripmn/SpringRepoServiceTest.java
@@ -1,6 +1,9 @@
 package tripmn;
 
+import java.util.Date;
+
 import com.tripmn.repository.UserRepository;
+import com.tripmn.utils.PlatformUtils;
 
 public class SpringRepoServiceTest {
 
@@ -8,5 +11,9 @@ public class SpringRepoServiceTest {
 	
 	public void test(){
 		repository.findById(1L, true);
+	}
+	
+	public static void main(String a[]){
+		System.out.println(PlatformUtils.formatDate(new Date(), "yyyy-MM-dd HH:mm:ss"));
 	}
 }
